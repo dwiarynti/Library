d42f9c1fe626145b36ec264db5d77720ee64fac2
Dwi Ariyanti <dwiariyanti121@gmail.com>
2017-04-11 12:01:07 +0700

    init

---
+++ NodeJS/NodeJS/ReadWrite.js
@@ -0,0 +1,10 @@
+﻿var mongooseDrv = require('mongoose');
+//connection with the MongooDB
+
+mongooseDrv.connect('mongodb://192.168.1.99/Test_DB');
+var db = mongooseDrv.Connection;
+
+var student = mongooseDrv.Schema({
+    Name: String,
+    Image: { data: Buffer, contentType: String }
+});
